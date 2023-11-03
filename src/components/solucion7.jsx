ff451017ec3711c62f07ebb02432a5d84bf6e371
Marce Benitez <106833298+marcebenitez2@users.noreply.github.com>
2023-11-03 08:52:11 -0300

    Update solucion7.jsx

--- src/components/solucion7.jsx
+++ src/components/solucion7.jsx
@@ -1,15 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 
-const localidades = [
-  { id: 1, nombre: "Rafaela" },
-  { id: 2, nombre: "Rosario" },
-  { id: 3, nombre: "Ramallo" },
-  { id: 4, nombre: "Chascomus" },
-  { id: 5, nombre: "Bigand" },
-  { id: 6, nombre: "San Nicolas" },
-  { id: 7, nombre: "Buenos Aires" },
-];
 
 const meses = [
   { id: 1, nombre: "Enero" },
@@ -32,6 +23,22 @@ function Solucion7() {
   const [mes, setMes] = useState(5);
   const [anio, setAnio] = useState(2022);
   const [total, setTotal] = useState(0);
+  const [localidades, setLocalidades] = useState([])
+
+  useEffect(() => {
+    setLocalidades(fetchLocalidades())
+  }, [])
+
+  async function fetchLocalidades() {
+    try {
+      const response = await fetch()
+      const respuesta = await response.json()
+      console.log(respuesta)
+      return respuesta
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   const calcular = async () => {
     const info = {
@@ -63,95 +70,99 @@ function Solucion7() {
   };
 
   return (
-    <main className="w-2/5 flex flex-col text-white gap-10 pt-6">
-      <div className="flex w-full justify-between">
-        <div className="flex flex-col">
-          <label>Localidad</label>
-          <select
-            value={localidad}
-            onChange={(e) => setLocalidad(Number(e.target.value))}
-            className="text-black"
-          >
-            {localidades.map((x) => (
-              <option value={x.id}>{x.nombre}</option>
-            ))}
-          </select>
-        </div>
-        <div className="flex flex-col">
-          <label>Mes</label>
-          <select
-            className="text-black"
-            value={mes}
-            onChange={(e) => setMes(e.target.value)}
-          >
-            <option value="1">Enero</option>
-            <option value="2">Febrero</option>
-            <option value="3">Marzo</option>
-            <option value="4">Abril</option>
-            <option value="5">Mayo</option>
-            <option value="6">Junio</option>
-            <option value="7">Julio</option>
-            <option value="8">Agosto</option>
-            <option value="9">Septiembre</option>
-            <option value="10">Octubre</option>
-            <option value="11">Noviembre</option>
-            <option value="12">Diciembre</option>
-          </select>
-        </div>
-        <div className="flex flex-col">
-          <label>Año</label>
-          <input
-            className="text-black"
-            value={anio}
-            onChange={(e) => setAnio(e.target.value)}
-          />
-        </div>
-        <button className=" text-2xl text-cyan-400" onClick={calcular}>
-          Calcular
-        </button>
-      </div>
-      <aside>
-        {total ? (
-          <div className="mb-8">
-            <p>
-              Localidad Origen o destino: {localidades[localidad - 1].nombre}
-            </p>
-            <p>
-              En el mes de: {meses[mes - 1].nombre} {anio}
-            </p>
+    <div>
+      {localidades ? <main className="w-2/5 flex flex-col text-white gap-10 pt-6">
+        <div className="flex w-full justify-between">
+          <div className="flex flex-col">
+            <label>Localidad</label>
+            <select
+              value={localidad}
+              onChange={(e) => setLocalidad(Number(e.target.value))}
+              className="text-black"
+            >
+              {localidades.map((x) => (
+                <option value={x.id}>{x.nombre}</option>
+              ))}
+            </select>
           </div>
-        ) : null}
-        <table className="w-full text-center">
-          <thead>
-            <tr className="text-cyan-400">
-              <th>Dia</th>
-              <th>Origen</th>
-              <th>Destino</th>
-              <th>Kilos</th>
-            </tr>
-          </thead>
-          <tbody>
-            {viajes.map((x, index) => (
-              <tr key={index}>
-                <td>{parseInt(x.fecViaje.split("-")[2], 10)}</td>
-                <td>
-                  {localidades[parseInt(x.locOrigen) - 1].id === localidad
-                    ? " "
-                    : localidades[parseInt(x.locOrigen) - 1].nombre}
-                </td>
-                <td>
-                  {localidades[parseInt(x.locDestino) - 1].id === localidad
-                    ? " "
-                    : localidades[parseInt(x.locDestino) - 1].nombre}
-                </td>
-                <td>{x.cantKg}</td>
+          <div className="flex flex-col">
+            <label>Mes</label>
+            <select
+              className="text-black"
+              value={mes}
+              onChange={(e) => setMes(e.target.value)}
+            >
+
+              <option value="1">Enero</option>
+              <option value="2">Febrero</option>
+              <option value="3">Marzo</option>
+              <option value="4">Abril</option>
+              <option value="5">Mayo</option>
+              <option value="6">Junio</option>
+              <option value="7">Julio</option>
+              <option value="8">Agosto</option>
+              <option value="9">Septiembre</option>
+              <option value="10">Octubre</option>
+              <option value="11">Noviembre</option>
+              <option value="12">Diciembre</option>
+            </select>
+          </div>
+          <div className="flex flex-col">
+            <label>Año</label>
+            <input
+              className="text-black"
+              value={anio}
+              onChange={(e) => setAnio(e.target.value)}
+            />
+          </div>
+          <button className=" text-2xl text-cyan-400" onClick={calcular}>
+            Calcular
+          </button>
+        </div>
+        <aside>
+          {total ? (
+            <div className="mb-8">
+              <p>
+                Localidad Origen o destino: {localidades[localidad - 1].nombre}
+              </p>
+              <p>
+                En el mes de: {meses[mes - 1].nombre} {anio}
+              </p>
+            </div>
+          ) : null}
+          <table className="w-full text-center">
+            <thead>
+              <tr className="text-cyan-400">
+                <th>Dia</th>
+                <th>Origen</th>
+                <th>Destino</th>
+                <th>Kilos</th>
               </tr>
-            ))}
-            <p className="mt-8">TOTAL: {total} kg</p>
-          </tbody>
-        </table>
-      </aside>
-    </main>
+            </thead>
+            <tbody>
+              {viajes.map((x, index) => (
+                <tr key={index}>
+                  <td>{parseInt(x.fecViaje.split("-")[2], 10)}</td>
+                  <td>
+                    {localidades[parseInt(x.locOrigen) - 1].id === localidad
+                      ? " "
+                      : localidades[parseInt(x.locOrigen) - 1].nombre}
+                  </td>
+                  <td>
+                    {localidades[parseInt(x.locDestino) - 1].id === localidad
+                      ? " "
+                      : localidades[parseInt(x.locDestino) - 1].nombre}
+                  </td>
+                  <td>{x.cantKg}</td>
+                </tr>
+              ))}
+              <p className="mt-8">TOTAL: {total} kg</p>
+            </tbody>
+          </table>
+        </aside>
+      </main> : null}
+    </div>
+
   );
 }
 
